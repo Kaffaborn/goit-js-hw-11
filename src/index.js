557894206babd5db6a83ac92fd147b2e3606a884
Andrii Ivaniuk <andrey.ivanjuk@gmail.com>
2023-04-06 20:38:24 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import Notiflix from 'notiflix';
-import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from "axios";
+import "./styles.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const BASE_URL = 'https://pixabay.com/api/' ;
 const API_KEY = '35073807-6e0c78c0f1d8a4d5139e72432';
-let searchQuerry = '';
+let searchQuery = '';
 let currentPage = 1;
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
@@ -13,74 +21,81 @@ const refs = {
 }
 
 refs.searchForm.addEventListener('submit', onSearch);
-
-
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
-    resetPage();
-    e.preventDefault();
-    clearContainer();
-    
-    searchQuerry = e.currentTarget.elements.searchQuery.value.trim();
-    
-    const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuerry}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
-        if (searchQuerry === '') {
-            // refs.loadMoreBtn.classList.add('is-hidden');
-            Notiflix.Notify.failure("Enter something.");
-            }
-        else{
-            fetchImg(url).then(cards => {
-            if (cards.total === 0) {
-            // refs.loadMoreBtn.classList.add('is-hidden');
-            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            }
-        else{
-            Notiflix.Notify.success(`Hooray! We found ${cards.totalHits} images.`);
-            }
-        })
-}
-}
+  resetPage();
+  e.preventDefault();
+  clearGalleryContainer();
+  searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+  const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
 
-async function fetchImg(url){
+  if (searchQuery === '') {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    Notify.failure("Please enter something.");
+  } else {
+    fetchImages(url).then(hits => {
+      if (hits.total === 0) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        } else {
+          Notify.success(`Hooray! We found ${hits.totalHits} images.`);
+        }
+    })
+  }  
+};
+
+function clearGalleryContainer() {
+    refs.galleryCard.innerHTML = '';
+  };
+
+function onLoadMore() {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
+  fetchImages(url);
+};
+
+async function fetchImages(url){
   try {
-    const response = await axios(name);
-    const cards = response.data;
-    refs.galleryCard.insertAdjacentHTML('beforeend', renderCards(cards));
-    currentPage +=1;
-    // refs.loadMoreBtn.classList.remove('is-hidden');
-    // lightbox.refresh();
-    return cards;
-  } catch{
-    // refs.loadMoreBtn.classList.add('is-hidden');
-    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+    const response = await axios(url);
+    const hits = await response.data;
+    refs.galleryCard.insertAdjacentHTML('beforeend', renderCards(hits));
+    currentPage += 1;
+    refs.loadMoreBtn.classList.remove('is-hidden');
+    lightBox.refresh();
+    return hits;
+  } catch {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    Notify.failure("We're sorry, but you've reached the end of search results.");
   }
 }
 
-function renderCards(cards){
-    return cards.hits.map(({webformatURL,largeImageURL,tags,likes,views,comments, downloads}) => {
-return `<div class="photo-card">
-<a class='gallery__link' href='${largeImageURL}'><img class='gallery__image' src="${webformatURL}" alt="${tags}" loading="lazy" width='360' height='260'/></a>
-<div class="info">
-  <p class="info-item">
-    <b>Likes:${likes}</b>
-  </p>
-  <p class="info-item">
-    <b>Views:${views}</b>
-  </p>
-  <p class="info-item">
-    <b>Comments:${comments}</b>
-  </p>
-  <p class="info-item">
-    <b>Downloads:${downloads}</b>
-  </p>
-</div>
-</div>`
-    }).join('')
-}
+function renderCards(hits) {
+  return hits.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments,  downloads}) => {
+    return `
+    
+    <a class="gallery__link" href='${largeImageURL}'>
+    <div class="gallery__card">
+    <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info__item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info__item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info__item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info__item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+    </div>
+    </a>`
+  }).join('');
+};
 
-function clearContainer(){
-  refs.galleryCard.innerHTML ='';
-}
+
 
 function resetPage(){
   currentPage = 1;
